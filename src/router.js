7907afe1bc0751eb3e67ad9ff2d56dfad6250271
Elbay Malik <eleo.666@gmail.com>
2023-06-11 22:35:28 +0200

    set the routes for the app

--- src/router.js
+++ src/router.js
@@ -2,3 +2,40 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 Vue.use(Router)
+
+import Home from './views/Home.vue'
+import ProductList from './views/ProductList.vue'
+import ProductDetails from './views/ProductDetails.vue'
+import Cart from './views/Cart.vue'
+import Checkout from './views/Checkout.vue'
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/products',
+      name: 'products',
+      component: ProductList
+    },
+    {
+      path: '/product/:id',
+      name: 'product-details',
+      component: ProductDetails
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: Cart
+    },
+    {
+      path: '/checkout',
+      name: 'checkout',
+      component: Checkout
+    }
+  ]
+})
