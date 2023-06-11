277ba156706b612798bd1e521d1872482d1b5169
Elbay Malik <eleo.666@gmail.com>
2023-06-11 22:48:42 +0200

    import router and tell Vue to use it

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app');
+
+import Vue from 'vue';
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app');
